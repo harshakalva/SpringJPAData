66714b594f36d7af750e2a0afcfaeeaf40168258
= <harsha.kalva@gmail.com>
2024-01-11 19:36:04 -0600

    TestController bad code

--- src/main/java/com/springjpadata/TestController.java
+++ src/main/java/com/springjpadata/TestController.java
@@ -1,2 +1,20 @@
-package com.springjpadata;public class TestController {
+package com.springjpadata;
+
+public class TestController {
+    public static final Integer ONE =  Integer.valueOf(1);
+    public static final String ONE_STRING = "1";
+    private Integer myInteger;
+    private String myString;
+    public boolean validate() {
+        System.out.println(ONE);
+        if(myString.equals(ONE_STRING)) {
+            System.out.println("String are equal");
+        }
+        if(myInteger.equals(ONE)) {
+            System.out.println("Equals");
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
