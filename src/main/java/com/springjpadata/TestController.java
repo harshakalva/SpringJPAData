678e39235210483632024557e6036e3b6eb2e3df
= <harsha.kalva@gmail.com>
2024-01-13 09:21:16 -0600

    commit 8

--- src/main/java/com/springjpadata/TestController.java
+++ src/main/java/com/springjpadata/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
         System.out.println(ONE);
         if(myString.equals(ONE_STRING)) {
             System.out.println("String are equal");
-            System.out.println("Pull Request 1 update 7");
+            System.out.println("Pull Request 1 update 8");
         }
         if(myInteger.equals(ONE)) {
             System.out.println("Equals");
