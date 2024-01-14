e9260245573fc8918508b3dbaa7379e7e446236c
Harsha Kalva <harsha.kalva@gmail.com>
2024-01-13 22:21:48 -0600

    Update TestController.java

--- src/main/java/com/springjpadata/TestController.java
+++ src/main/java/com/springjpadata/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
         System.out.println(ONE);
         if(myString.equals(ONE_STRING)) {
             System.out.println("String are equal");
-            System.out.println("Pull Request 1 update 14");
+            System.out.println("Pull Request 1 update 15");
         }
         if(myInteger.equals(ONE)) {
             System.out.println("Equals");
